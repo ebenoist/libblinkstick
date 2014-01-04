2ad9c098e2198234b63de1c966c599dce3521781
Erik Benoist <ebenoist@groupon.com>
2014-01-04 14:55:50 -0600

    Iterate through all devices and get info

--- src/blinkstick.c
+++ src/blinkstick.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <libusb.h>
 
+void print_device(libusb_device *device, int i) {
+  struct libusb_device_descriptor desc;
+  int result = libusb_get_device_descriptor(device, &desc);
+
+  printf("Device: %d\n", i);
+  printf("--------------\n");
+  if (result >= 0) {
+    printf("%04x:%04x (bus %d, device %d)\n",
+        desc.idVendor, desc.idProduct,
+        libusb_get_bus_number(device), libusb_get_device_address(device));
+  } else {
+    printf("Failed to read\n");
+  }
+  printf("--------------\n\n");
+}
+
 int main() {
   libusb_device **devices;
   libusb_context *context = NULL;
@@ -8,12 +24,17 @@ int main() {
   int result;
   ssize_t device_count;
 
+  printf("Initializing USB context\n");
   result = libusb_init(&context);
-  libusb_set_debug(context, 3);
+  // libusb_set_debug(context, 3);
   device_count = libusb_get_device_list(context, &devices);
 
-  printf("%d was the result.", result);
   printf("%d devices found\n", (int)device_count);
+
+  for(int i = 0; i < device_count; i++) {
+    libusb_device *device = devices[i];
+    print_device(device, i);
+  }
 
   return 0;
 }

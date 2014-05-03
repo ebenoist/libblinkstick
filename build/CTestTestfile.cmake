5078b8f6ffd792c7108a2f28a3fef72c2658cf77
Erik Benoist <ebenoist@groupon.com>
2014-05-03 14:38:49 -0500

    adding a target directory for better build control

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ebenoist/development/libblinkstick
-# Build directory: /Users/ebenoist/development/libblinkstick/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
--- test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ebenoist/development/libblinkstick/test
-# Build directory: /Users/ebenoist/development/libblinkstick/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

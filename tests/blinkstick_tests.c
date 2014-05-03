0c59ce224773dc906f3126fb9e867eab850529b3
Erik Benoist <ebenoist@groupon.com>
2014-05-03 11:59:58 -0500

    building out proper unit tests

--- tests/blinkstick_tests.c
+++ tests/blinkstick_tests.c
@@ -16,12 +16,25 @@ START_TEST (test_rgb_factory) {
 }
 END_TEST
 
+START_TEST (test_byte_representation) {
+  int red, green, blue;
+  red = 255;
+  green = 255;
+  blue = 255;
+
+  rgb_color *color = rgb_color_factory(red,green, blue);
+  ck_assert(color->bytes[0] == '\x01');
+  ck_assert_msg(color->bytes[1] == 'ff', "Got: %2x", color->bytes[1]);
+}
+END_TEST
+
 Suite * basic_suite (void) {
   Suite *s = suite_create ("Lib BlinkStick");
 
   /* Core test case */
   TCase *tc_core = tcase_create ("Core");
   tcase_add_test (tc_core, test_rgb_factory);
+  tcase_add_test (tc_core, test_byte_representation);
   suite_add_tcase (s, tc_core);
 
   return s;

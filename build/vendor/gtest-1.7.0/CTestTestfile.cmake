69630705e10dfd96c98badcfd404a0e895f2b519
Erik Benoist <ebenoist@groupon.com>
2014-05-04 17:04:53 -0500

    do not check in build files

--- build/vendor/gtest-1.7.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ebenoist/development/libblinkstick/vendor/gtest-1.7.0
-# Build directory: /Users/ebenoist/development/libblinkstick/build/vendor/gtest-1.7.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

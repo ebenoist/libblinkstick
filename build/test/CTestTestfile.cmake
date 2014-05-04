bec8c6bc0cdb7c622c9ec3e1092869f1bb2446d9
Erik Benoist <ebenoist@groupon.com>
2014-05-04 17:06:24 -0500

    oops a few more build files:

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ebenoist/development/libblinkstick/test
-# Build directory: /Users/ebenoist/development/libblinkstick/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(unit-tests "unit_tests")
